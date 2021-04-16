6670a32f09994dbd620141fa4235f3143a1d8450
Juan Manuel Burgues Schaab <securetux@gmail.com>
2021-04-16 18:08:42 -0300

    Adds Spring Project

---
+++ src/main/java/edu/utn/UEEDServer/UeedServerApplication.java
@@ -0,0 +1,13 @@
+package edu.utn.UEEDServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UeedServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UeedServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/utn/UEEDServer/UeedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utn.UEEDServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UeedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
