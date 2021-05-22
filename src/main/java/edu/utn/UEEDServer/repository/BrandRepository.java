e4af826ac7b89d99e4fc0ad8898527ab4992f981
Juan Manuel Burgues Schaab <securetux@gmail.com>
2021-05-22 19:55:19 -0300

    Controllers and Servicies reformatting and cleaning

--- src/main/java/edu/utn/UEEDServer/repository/BrandRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.utn.UEEDServer.repository;
-
-import edu.utn.UEEDServer.model.Brand;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BrandRepository extends JpaRepository<Brand,Integer> {
-}
--- src/main/java/edu/utn/UEEDServer/repository/ModelRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.utn.UEEDServer.repository;
-
-import edu.utn.UEEDServer.model.Model;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModelRepository extends JpaRepository<Model,Integer> {
-}
