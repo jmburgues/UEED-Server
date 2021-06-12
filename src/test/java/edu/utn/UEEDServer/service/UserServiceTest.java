710b17947173dde8fe2df6a1eb032708c5d9b5b0
federicovinas7@gmail.com <federicovinas7@gmail.com>
2021-06-12 17:30:54 -0300

    UserServiceTest

--- src/test/java/edu/utn/UEEDServer/service/UserServiceTest.java
+++ src/test/java/edu/utn/UEEDServer/service/UserServiceTest.java
@@ -1,2 +1,39 @@
-package edu.utn.UEEDServer.service;public class UserServiceTest {
+package edu.utn.UEEDServer.service;
+
+import edu.utn.UEEDServer.model.User;
+import edu.utn.UEEDServer.repository.UserRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static edu.utn.UEEDServer.utils.TestUtils.aUser;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class UserServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    UserService userService;
+
+    @Before
+    public void setUp(){
+        initMocks(this);
+        this.userService = new UserService(userRepository);
+    }
+
+    @Test
+    public void findByUserAndPassTest(){
+        String username = "user";
+        String password = "pass";
+
+        when(userRepository.findByUserAndPass(username,password)).thenReturn(aUser());
+
+        User user = userService.findByUserAndPass(username,password);
+
+        Assert.assertEquals(aUser(),user);
+    }
+
 }
