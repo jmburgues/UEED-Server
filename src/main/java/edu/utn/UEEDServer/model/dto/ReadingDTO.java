616d46ec7c23decf2303dbcaf1db5e32ff87a1bc
Juan Manuel Burgues Schaab <securetux@gmail.com>
2021-06-15 12:13:41 -0300

    Adds JsonProperty to ReadingDTO and noSql fixes

--- src/main/java/edu/utn/UEEDServer/model/dto/ReadingDTO.java
+++ src/main/java/edu/utn/UEEDServer/model/dto/ReadingDTO.java
@@ -1,5 +1,6 @@
 package edu.utn.UEEDServer.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +13,12 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class ReadingDTO {
+    @JsonProperty("date")
     private LocalDateTime readDate;
+    @JsonProperty("value")
     private Float totalKw;
+    @JsonProperty("serialNumber")
     private String meterSerialNumber;
+    @JsonProperty("password")
     private String password;
 }
